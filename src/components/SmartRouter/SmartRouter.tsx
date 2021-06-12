33e5dc7d6fc6bbe33d26ba92c1d1d4e5dca1f839
Pavel Lozhkin <pasha26k@ya.ru>
2021-06-12 17:11:00 +0300

    Добавлена типизация и пропсы для SmartRouter

--- src/components/SmartRouter/SmartRouter.tsx
+++ src/components/SmartRouter/SmartRouter.tsx
@@ -1,8 +1,20 @@
-import React, {FC} from "react";
+import React, { FC, ReactNode } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { LayoutProperties } from '../../types/layouts.types';
+import { RouteProperties } from '../../types/routes.types';
 
+interface SmartRouterProperties<T = ReactNode> {
+  appRoute: RouteProperties;
+  layout: FC<LayoutProperties>;
+  route: RouteComponentProps<T>;
+}
 
-const SmartRouter: FC = ({appRoute, layout: Layout, route}) => {
-  return <Layout Page={appRoute.page} route={route}/>;
+const SmartRouter: FC<SmartRouterProperties> = ({
+  appRoute,
+  layout: Layout,
+  route,
+}) => {
+  return <Layout Page={appRoute.page} route={route} />;
 };
 
 export default SmartRouter;
