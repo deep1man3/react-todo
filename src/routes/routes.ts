caf6ffbb20829a873de796624868e7f1b3372743
Pavel Lozhkin <pasha26k@ya.ru>
2021-06-12 17:39:05 +0300

    Добавлена конфигурация privateRoutes в rootRoutes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,17 @@
 import { publicRoutes } from './public.routes';
 import DefaultLayout from '../layouts/DefaultLayout';
 import { RootRouteProperties } from '../types/routes.types';
+import { privateRoutes } from "./private.routes";
 
 const rootRoutes: RootRouteProperties[] = [
   {
     routesList: publicRoutes,
     layout: DefaultLayout,
   },
+  {
+    routesList: privateRoutes,
+    layout: DefaultLayout,
+  }
 ];
 
 export default rootRoutes;
