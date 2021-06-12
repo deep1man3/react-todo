4e6d734d7193e4b526e2fe31acf22aaa67745069
Pavel Lozhkin <pasha26k@ya.ru>
2021-06-12 16:13:20 +0300

    Добавлен вывод компонента Appbar в DefaultLayout

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Appbar from "../../components/Appbar";
 
 const DefaultLayout = () => {
     return (
         <div>
-        
+            <Appbar>
+                DefaultLayout
+            </Appbar>
         </div>
     )
 }
