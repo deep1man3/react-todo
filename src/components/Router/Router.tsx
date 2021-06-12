a31e3e3d1e8adcef5a45c94a89368915fc216054
Pavel Lozhkin <pasha26k@ya.ru>
2021-06-12 17:11:37 +0300

    Добавлена логика генерации роутов в компоненте Router

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,15 +1,32 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "../../pages/Home";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import rootRoutes from '../../routes';
+import SmartRouter from '../SmartRouter/SmartRouter';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route>404</Route>
+        {rootRoutes.map((rootRoute) => {
+          const { routes: routesList, layout } = rootRoute;
+          {
+            return routesList.map((appRoute) => (
+              <Route
+                key={appRoute.path}
+                exact={appRoute.exact}
+                path={appRoute.path}
+                render={(route) => (
+                  <SmartRouter
+                    route={route}
+                    layout={layout}
+                    appRoute={appRoute}
+                  />
+                )}
+              />
+            ));
+          }
+        })}
+        ;<Route>404</Route>
       </Switch>
     </BrowserRouter>
   );
