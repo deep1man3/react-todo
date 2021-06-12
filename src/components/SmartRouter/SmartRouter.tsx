70ff94eee0c7d8386f937ccc9ea1de458c8e27ce
Pavel Lozhkin <pasha26k@ya.ru>
2021-06-12 16:12:52 +0300

    Добавлены пропсы: layout, appRoute, route в компонент SmartRouter

--- src/components/SmartRouter/SmartRouter.tsx
+++ src/components/SmartRouter/SmartRouter.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {FC} from "react";
 
-const SmartRouter = () => {
-  return <div>SmartRouter</div>;
+
+const SmartRouter: FC = ({appRoute, layout: Layout, route}) => {
+  return <Layout Page={appRoute.page} route={route}/>;
 };
 
 export default SmartRouter;
