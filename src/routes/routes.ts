0628efba5fec4befe6d37af5c311b26a134c052e
Pavel Lozhkin <pasha26k@ya.ru>
2021-06-12 20:08:51 +0300

    Добавлен параметр privacy в root Routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,16 +1,18 @@
 import { publicRoutes } from './public.routes';
 import DefaultLayout from '../layouts/DefaultLayout';
-import { RootRouteProperties } from '../types/routes.types';
+import { PrivacyType, RootRouteProperties } from '../types/routes.types';
 import { privateRoutes } from './private.routes';
 
 const rootRoutes: RootRouteProperties[] = [
   {
     routesList: publicRoutes,
     layout: DefaultLayout,
+    privacy: PrivacyType.Public,
   },
   {
     routesList: privateRoutes,
     layout: DefaultLayout,
+    privacy: PrivacyType.Private,
   },
 ];
 
