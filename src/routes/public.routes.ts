a816f071db2981b324e250f8731e8a77bc33fbec
Pavel Lozhkin <pasha26k@ya.ru>
2021-06-12 17:10:23 +0300

    Добавлена типизация для publicRoutes

--- src/routes/public.routes.ts
+++ src/routes/public.routes.ts
@@ -1,6 +1,7 @@
 import Home from "../pages/Home";
+import {RouteProperties} from "../types/routes.types";
 
-export const publicRoutes = [
+export const publicRoutes: RouteProperties[] = [
   {
     label: "Главная",
     exact: true,
