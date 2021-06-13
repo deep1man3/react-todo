de11e7866c629b2ca4ed9a9bcc4db2d2c6416ada
Pavel Lozhkin <pasha26k@ya.ru>
2021-06-13 17:05:56 +0300

    Реформат кода App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import AppRouter from "./components/AppRouter";
+import React from 'react';
+import AppRouter from './components/AppRouter';
 
 const App = () => {
   return <AppRouter />;
