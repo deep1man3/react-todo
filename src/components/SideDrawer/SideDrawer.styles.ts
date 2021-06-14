f62131376cd95c7aa1f62ce7c25a79aa44a4971c
Pavel Lozhkin <pasha26k@ya.ru>
2021-06-14 17:22:34 +0300

    Изменена ширина drawerPaper с 280 на theme.drawer.width получаемую с файла конфигурации темы theme.ts

--- src/components/SideDrawer/SideDrawer.styles.ts
+++ src/components/SideDrawer/SideDrawer.styles.ts
@@ -1,11 +1,11 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useStyles = makeStyles((theme) => ({
+export const useStyles = makeStyles((theme: any) => ({
   root: {
     display: 'flex'
   },
   drawerPaper: {
-    width: 280,
+    width: theme.drawer.width,
     padding: theme.spacing(3)
   },
 }));
